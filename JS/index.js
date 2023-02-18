035752cafe05a7bad0f8fbd54f35ea690728a23c
MD ALAMIN A <86530759+Alamin0x01@users.noreply.github.com>
2023-02-18 21:23:53 +0600

    Blog page custumize

--- JS/index.js
+++ JS/index.js
@@ -126,19 +126,3 @@ function displayData(titleName, total) {
 function disabledButton(id) {
   document.getElementById(id).setAttribute("disabled", true);
 }
-
-// var colorChange = $('.container .card .face.face1');
-// var original = colorChange.css('background');
-// var colors = ['#AC4EC6', ' #A4C64E', '#C6704E', '#C6AC4E', '#C64E68','#C64EA4','#4B99C2'];
-
-// colorChange.hover(function () {
-// // defines location of new color in the colors array
-//     var hexLocation = Math.floor(Math.random() * colors.length);
-//     var newcolor = colors[hexLocation];
-
-// //replaces background property with new color when hovering
-//     $(this).css('background', newcolor);
-// }, function() {
-//  //replaces background property with original color when NOT hovering
-//     $(this).css('background', original);
-// });
